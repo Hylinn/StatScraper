6bf7576efe5a4502fb04c8110dec8139d5493087
Hylinn Taggart <hylinn.taggart+git@gmail.com>
2015-08-07 14:42:02 -0700

    Created front-end web page.
    This is just the minimum viable product.

---
+++ src/main/webapp/resources/js/app.js
@@ -0,0 +1,38 @@
+var app = angular.module('app', ['ui.grid']);
+
+app.controller('StatsController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.gridOptions = {
+        enableSorting: true,
+        columnDefs: [
+            { name: 'League', field: 'leagueSeasonDivision.leagueSeason.league.name' },
+            { name: 'Season', field: 'leagueSeasonDivision.leagueSeason.season.timeOfYear.name'},
+            { name: 'Year', field: 'leagueSeasonDivision.leagueSeason.season.year'},
+            { name: 'Division', field: 'leagueSeasonDivision.division.name' },
+            { name: 'Team', field: 'team.name'}
+        ]
+    };
+
+    $http.get('http://statscraper.elasticbeanstalk.com:8080/player/teams?id=10').then(
+        function(response) {
+            $scope.gridOptions.data = response.data;
+        },
+        function(response) {
+
+        }
+    );
+
+    $scope.search = function(id) {
+        $http.get('http://statscraper.elasticbeanstalk.com:8080/player/teams?id=' + id).then(
+            function(response) {
+                $scope.gridOptions.data = response.data;
+            },
+            function(response) {
+
+            }
+        );
+    }
+}]);
+
+
+
